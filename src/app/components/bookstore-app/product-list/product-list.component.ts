d2d736d50ed6e7b28acd12d25b94405fb6858396
Bruno Marques <brunoaffonsomarques@gmail.com>
2022-06-07 09:32:41 -0300

    1.1

---
+++ src/app/components/bookstore-app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { BookService } from './product-list.component.serice';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  livros: any;
+  bookService : BookService
+
+  constructor( bookService: BookService) {
+    this.bookService = bookService;
+   }
+
+  ngOnInit(): void {
+    this.livros = this.bookService.getBook().subscribe((data => {
+      this.livros = data;
+    }))
+  }
+
+
+
+}
